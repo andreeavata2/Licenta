5d45aec94e72bcc767bfbc95aae19d209a53db7d
Andreea Vatamanelu <andreea.vata2@gmail.com>
2020-06-13 15:59:35 +0300

    add small things at the home page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,39 +1,47 @@
-import React, { Component }  from 'react'
+import React, { Component } from 'react'
 // import "../../assets/css/bootstrap.css";
 import "../../assets/css/homePage.css";
+// import 'font-awesome/css/font-awesome.min.css';
+import "../../../node_modules/font-awesome/css/font-awesome.min.css"
+// import FontAwesome from 'react-fontawesome/font-awesome/css/font-awesome.min.css';
+
 import imageBackground1 from "../../assets/img/teacher-and-student.jpg";
 import imageBackground2 from "../../assets/img/header-3.jpg";
-
+import face1 from "../../assets/img/faces/face_1.jpg";
+import face2 from "../../assets/img/faces/face_2.jpg";
+import face3 from "../../assets/img/faces/face_3.jpg";
 
 class Landing extends Component {
     render() {
         return (
             <>
                 <div>
-                    <div className="section section-header">
-                        <div className="parallax filter filter-color-red">
-                            <div className="image"
-                                style={{
-                                    backgroundImage: "url(" + imageBackground1 + ")",
-                                    backgroundPosition: 'center',
-                                    backgroundSize: 'cover',
-                                    backgroundRepeat: 'no-repeat'
-                                }}
-                            >
-                            </div>
-
-                            <div className="container">
-                                <div className="content">
-                                    <div className="title-area">
-                                        <h1 className="title-modern">License Management</h1>
-                                        <div className="separator line-separator">♦</div>
-                                        <h3>Probably the most useful site for organizing licenses!</h3>
-                                    </div>
+                    <din className="main-page">
+                        <div className="section section-header">
+                            <div className="parallax filter filter-color-red">
+                                <div className="image"
+                                    style={{
+                                        backgroundImage: "url(" + imageBackground1 + ")",
+                                        backgroundPosition: 'center',
+                                        backgroundSize: 'cover',
+                                        backgroundRepeat: 'no-repeat'
+                                    }}
+                                >
                                 </div>
 
+                                <div className="container">
+                                    <div className="content">
+                                        <div className="title-area">
+                                            <h1 className="title-modern">License Management</h1>
+                                            <div className="separator line-separator">♦</div>
+                                            <h3>Probably the most useful site for organizing licenses!</h3>
+                                        </div>
+                                    </div>
+
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </din>
 
 
                     <div className="section">
@@ -69,9 +77,12 @@ class Landing extends Component {
                                 </div>
                                 <div className="col-md-4">
                                     <div className="info-icon">
-                                        <div className="icon text-danger">
-                                            <i className="pe-7s-music"></i>
+                                        {/* <div className="icon text-danger"> */}
+                                        <div>
+                                            {/* <i className="pe-7s-music"></i> */}
+                                            <i className="fas fa-music"></i>
                                         </div>
+
                                         <h3>Music</h3>
                                         <p className="description">We like to present the world with our work, so we make sure we spread the
                         word regarding our clothes.</p>
@@ -112,8 +123,10 @@ class Landing extends Component {
                                                         <div className="card card-member">
                                                             <div className="content">
                                                                 <div className="avatar avatar-danger">
-                                                                    <img alt="..." className="img-circle"
-                                                                        src="assets/img/faces/face_1.jpg" />
+                                                                    {/* <img alt="..." className="img-circle"
+                                                                        src= "../../assets/img/faces/face_1.jpg" /> */}
+                                                                    <img src={face1} />;
+
                                                                 </div>
                                                                 <div className="description">
                                                                     <h3 className="title">Tina</h3>
@@ -130,8 +143,9 @@ class Landing extends Component {
                                                         <div className="card card-member">
                                                             <div className="content">
                                                                 <div className="avatar avatar-danger">
-                                                                    <img alt="..." className="img-circle"
-                                                                        src="assets/img/faces/face_4.jpg" />
+                                                                    {/* <img alt="..." className="img-circle"
+                                                                        src="assets/img/faces/face_4.jpg" /> */}
+                                                                    <img src={face2} />;
                                                                 </div>
                                                                 <div className="description">
                                                                     <h3 className="title">Andrew</h3>
@@ -148,8 +162,9 @@ class Landing extends Component {
                                                         <div className="card card-member">
                                                             <div className="content">
                                                                 <div className="avatar avatar-danger">
-                                                                    <img alt="..." className="img-circle"
-                                                                        src="assets/img/faces/face_3.jpg" />
+                                                                    {/* <img alt="..." className="img-circle"
+                                                                        src="assets/img/faces/face_3.jpg" /> */}
+                                                                    <img src={face3} />;
                                                                 </div>
                                                                 <div className="description">
                                                                     <h3 className="title">Michelle</h3>
